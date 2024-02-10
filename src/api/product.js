dbeaf3e2f84fba1495d651b1fcb4cbf3f7f1d781
gorojack <1937047112@qq.com>
2024-02-10 10:40:02 +0800

    API: Product info, sku and update

--- src/api/product.js
+++ src/api/product.js
@@ -7,3 +7,27 @@ export function getProductPage(params) {
     params
   })
 }
+
+export function getProductInfo(params) {
+  return request({
+    url: '/product/info',
+    method: 'get',
+    params
+  })
+}
+
+export function getSku(params) {
+  return request({
+    url: '/product/sku',
+    method: 'get',
+    params
+  })
+}
+
+export function updateProduct(data) {
+  return request({
+    url: '/product/update',
+    method: 'post',
+    data
+  })
+}
